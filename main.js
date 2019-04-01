58eb38cb778048d987b925ed2a6cb244a86d95b4
NuoWenLei <nuowen.lei@concordacademy.org>
2019-04-01 11:52:58 -0400

    Start Game

--- main.js
+++ main.js
@@ -1,13 +1,29 @@
 let canvas = document.getElementById('main');
 let c = canvas.getContext('2d');
+canvas.width = innerWidth;
+canvas.height = innerHeight;
 
+let mouse = {
+    x:0,
+    y:0
+}
+let pos = new Vector(60,60);
+let vel = new Vector(.5, .2);
 function init() {
-  alert('Hello World!');
+    alert('Hello World!');
+    console.log(pos.toString());
 
     update();
 }
 
 function update() {
+    c.clearRect(0,0,canvas.width, canvas.height);
+
+    pos.addVector(vel);
+
+    c.beginPath();
+    c.arc(pos.x, pos.y, 50, 0, Math.PI*2, false);
+    c.stroke();
 
     requestAnimationFrame(update);
 }
@@ -15,3 +31,7 @@ function update() {
 window.addEventListener('load', function(event) {
     init();
 });
+window.addEventListener('mousemove', function(event){
+    mouse.x = event.x;
+    mouse.y = event.y;
+})
--- vector.js
+++ vector.js
@@ -1,7 +1,28 @@
 class Vector {
-    
-    constructor() {
+
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    addVector(vec){
+        this.x += vec.x;
+        this.y += vec.y;
+        return this;
+    }
+
+//Functions to write for HW
+    subVector(vec){
+        this.x -= vec.x;
+        this.y -= vec.y;
+        return this;
+    }
+
+    scale(s){
 
     }
 
+    toString() {
+        return '<' + this.x + ',' + this.y + '>';
+    }
 }
