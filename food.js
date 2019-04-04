2f2f24190d22669a58cf66d01f474cb40b04897d
NuoWenLei <nuowen.lei@concordacademy.org>
2019-04-04 10:14:44 -0400

    A hundred Circles

---
+++ food.js
@@ -0,0 +1,18 @@
+class Food extends Vector{
+
+    constructor(x, y, radius, color) {
+        super(x,y);
+        this.radius = radius;
+        this.color = color;
+    }
+
+    draw(context){
+        context.fillStyle = this.color;
+        context.beginPath();
+        context.arc(this.x,this.y,this.radius, 0, Math.PI*2, false);
+        context.closePath();
+        context.fill();
+    }
+
+}
+Object.assign(Food, Vector);
--- main.js
+++ main.js
@@ -7,26 +7,38 @@ let mouse = {
     x:0,
     y:0
 }
-let pos = new Vector(5, 7);
-let vel = new Vector(1, 1);
+
+let foods = [];
+
+let colorArray = ['#FF6633', '#FFB399', '#FF33FF', '#FFFF99', '#00B3E6',
+		  '#E6B333', '#3366E6', '#999966', '#99FF99', '#B34D4D',
+		  '#80B300', '#809900', '#E6B3B3', '#6680B3', '#66991A',
+		  '#FF99E6', '#CCFF1A', '#FF1A66', '#E6331A', '#33FFCC',
+		  '#66994D', '#B366CC', '#4D8000', '#B33300', '#CC80CC',
+		  '#66664D', '#991AFF', '#E666FF', '#4DB3FF', '#1AB399',
+		  '#E666B3', '#33991A', '#CC9999', '#B3B31A', '#00E680',
+		  '#4D8066', '#809980', '#E6FF80', '#1AFF33', '#999933',
+		  '#FF3380', '#CCCC00', '#66E64D', '#4D80CC', '#9900B3',
+		  '#E64D66', '#4DB380', '#FF4D4D', '#99E6E6', '#6666FF'];
+
+function colorRandom(){
+    return colorArray[Math.floor(Math.random()*colorArray.length)];
+}
 
 function init() {
-    console.log(pos.toString());
-    vel.scale(2);
-    console.log(vel.toString()); // <2, 2>
-    pos.subVector(vel);
-    console.log(pos.toString());  // <3, 5>
+    for(i = 0; i < 100; i ++){
+        foods.push(new Food(Math.random()*canvas.width, Math.random()*canvas.height, 20, colorRandom()));
+    }
+
     update();
 }
 
 function update() {
     c.clearRect(0,0,canvas.width, canvas.height);
 
-    pos.addVector(vel);
-
-    c.beginPath();
-    c.arc(pos.x, pos.y, 50, 0, Math.PI*2, false);
-    c.stroke();
+    for(var i = 0; i < foods.length; i++){
+        foods[i].draw(c);
+    }
 
     requestAnimationFrame(update);
 }
